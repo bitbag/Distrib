bb90c45984aae94c36ca5ccd059c615d32762ffe
bitbag <lufee9527@163.com>
2016-04-06 11:15:12 +0800

    'add_api_token_method_in_urls'

--- Distrib/Distrib/urls.py
+++ Distrib/Distrib/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework import routers
 from distrib_api import views
+from rest_framework.authtoken import views as restview
 
 router = routers.DefaultRouter()
 router.register(r'miss', views.miss_viewset)
@@ -14,6 +15,7 @@ router.register(r'host', views.host_viewset)
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(router.urls)),
+    url(r'^api/token/', restview.obtain_auth_token),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^index/', views.index),
     url(r'^rewis/', views.Write_to_redis),
