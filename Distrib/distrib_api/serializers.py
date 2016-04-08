519d589ae84b913f66ae1e1f0d0f3a5f7e125c66
bitbag <lufee9527@163.com>
2016-04-08 11:45:27 +0800

    'modify_Master_table'

--- Distrib/distrib_api/serializers.py
+++ Distrib/distrib_api/serializers.py
@@ -37,7 +37,7 @@ class Playbook_ser(serializers.HyperlinkedModelSerializer):
         fields = ('url','p_name','p_type')
 
 class Hosts_ser(serializers.HyperlinkedModelSerializer):
-    host_name = serializers.SlugRelatedField(queryset=Master.objects.all(), slug_field='host_ip')
+    host_name = serializers.SlugRelatedField(queryset=Masters.objects.all(), slug_field='host_ip')
     class Meta:
         model = Hosts
         fields = ('url','host_name','host_id','host_ip','host_group','remark')
