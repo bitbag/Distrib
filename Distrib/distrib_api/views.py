9bdde897effa19672a2b50e85543f6c8ef89819c
bitbag <lufee9527@163.com>
2016-04-27 15:38:18 +0800

    'modify_conflict'

--- Distrib/distrib_api/views.py
+++ Distrib/distrib_api/views.py
@@ -8,19 +8,12 @@ from .filters import *
 from datetime import datetime
 from django.core.cache import cache
 from distrib_api.monitor import WriteToEtcd
-
-# class seria_viewset(viewsets.ModelViewSet):
-#     queryset = Service_type.objects.all()
-#     serializer_class = Service_type_ser
-#     filter_backends = (backends.DjangoFilterBackend, )
-#     filter_class = ServiceTypeFilter
-
-# class ServiceTypeFilter(filters.FilterSet):
-#     ServiceName = filters.CharFilter(name='name')
-#
-#     class Meta:
-#         model = Service_type
-#         fields = ['name','alise']
+from rest_framework_filters.backends import DjangoFilterBackend
+from serializers import *
+from models import *
+from filters import *
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
 
 
 class miss_viewset(viewsets.ModelViewSet):
@@ -35,11 +28,6 @@ class submiss_viewset(viewsets.ModelViewSet):
     filter_backends = (backends.DjangoFilterBackend, )
     filter_class = SubMissFilter
 
-# class status_viewset(viewsets.ModelViewSet):
-#     queryset = Status.objects.all()
-#     serializer_class = Status_ser
-#     filter_backends = (backends.DjangoFilterBackend, )
-#     filter_class = StatusFilter
 
 class log_viewset(viewsets.ModelViewSet):
     queryset = log.objects.all()
@@ -64,90 +52,6 @@ class hosts_viewset(viewsets.ModelViewSet):
     serializer_class = Hosts_ser
     filter_backends = (backends.DjangoFilterBackend, )
     filter_class = HostsFilter
-
-
-
-def index(request):
-    return render(request,'index.html')
-
-
-def CreateMission(request):
-    if request.method == 'GET':
-        return render(request,'index.html')
-    elif request.method == 'POST':
-        HostList = list(set([ host for host in request.REQUEST.get('hostlist').split(',')]))
-        PlaybookList = list(set([playbook for playbook in request.REQUEST.get('playbook').split(',')]))
-        Version = request.REQUEST.get('version')
-        UniqueValue = datetime.now().strftime("%Y%m%d%H%M%S")
-        status = {
-            '0':'unexecuted',
-            '1':'executing',
-            '2':'executed',
-            '3':'failed',
-            '4':'unknown',
-        }
-        remark = request.REQUEST.get('remark')
-        try:
-            print '=====================ø™ º…˙≥…÷˜»ŒŒÒ==================='
-            Miss.objects.create(
-                hosts=HostList,
-                playbooks=PlaybookList,
-                version=Version,
-                status=status['0'],
-                uniquevalue=UniqueValue,
-                release_time=datetime.now(),
-                finish_time='unknown',
-                remark=remark
-            )
-            print '======================÷˜»ŒŒÒ“—…˙≥…====================='
-            for host in HostList:
-                hostdic = {'host':host,
-                           'status':status['0'],
-                           'playbook':PlaybookList
-                           }
-                try:
-                    SubMiss.objects.create(
-                        host=host,
-                        playbooks=PlaybookList,
-                        release_time=datetime.now(),
-                        finish_time='unknown',
-                        status=status['0'],
-                        uniquevalue=UniqueValue
-                    )
-                    print '======================◊”»ŒŒÒ–¥»Îmysql====================='
-                    try:
-                        WriteToEtcd(host,hostdic)
-                        print '======================◊”»ŒŒÒ“—–¥»Îetcd====================='
-                    except Exception,error:
-                        print '======================–¥»Îetcd ß∞‹ %s=====================' % error
-                except Exception,error:
-                    print "==============◊”»ŒŒÒ…˙≥… ß∞‹ %s===================" % error
-        except Exception,error:
-            print '====================Create mission failed! %s======================' % error
-        return render(request,'basex.html',{'sdf':(PlaybookList,HostList,Version)})
-    else:
-        return render(request,'basex.html',{'sdf':'use post or get'})
-
-
-def basex(request):
-    dicx = {}
-    if request.method == 'POST':
-        dicx = eval(json.dumps(request.POST))
-        try:
-            for k,val in dicx.items():
-                print k,val
-                if k != 'csrfmiddlewaretoken':
-                    cache.set(k,val)
-        except Exception,e:
-            return  render(request,'basex.html',{'sdf':e})
-    return render(request,'basex.html',{'sdf':dicx})
-=======
-from rest_framework_filters.backends import DjangoFilterBackend
-from serializers import *
-from models import *
-from filters import *
-from rest_framework import permissions
-from rest_framework.decorators import api_view, permission_classes
 
 class SearchViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.DjangoModelPermissions,)
@@ -213,76 +117,83 @@ class Ipv4NetworkViewSet(BaseViewSet):
     queryset=Ipv4Network.objects.all()
     serializer_class = IPv4NetworkSerializer
 
-# def CreateMission(request):
-#     if request.method == 'GET':
-#         return render(request,'index.html')
-#     elif request.method == 'POST':
-#         try:
-#             HostList = list(set([ host for host in request.REQUEST.get('hostlist').split(',')]))
-#             PlaybookList = list(set([playbook for playbook in request.REQUEST.get('playbook').split(',')]))
-#             Version = request.REQUEST.get('version')
-#             UniqueValue = datetime.now().strftime("%Y%m%d%H%M%S")
-#             status = {
-#                 '0':'unexecuted',
-#                 '1':'executing',
-#                 '2':'executed',
-#                 '3':'failed',
-#                 '4':'unknown',
-#             }
-#             remark = request.REQUEST.get('remark')
-#             try:
-#                 print r'=====================ÂºÄÂßãÁîüÊàê‰∏ª‰ªªÂä°==================='
-#                 Miss.objects.create(hosts=HostList,playbooks=PlaybookList,version=Version,status=status['0'],uniquevalue=UniqueValue,release_time=datetime.now(),finish_time='unknown',remark=remark)
-#                 print '======================‰∏ª‰ªªÂä°Â∑≤ÁîüÊàê====================='
-#                 if PlaybookList:
-#                     try:
-#                         for host in HostList:
-#                             SubMiss.objects.create(host=host,playbooks=PlaybookList,release_time=datetime.now(),finish_time='unknown',status=status['0'],uniquevalue=UniqueValue)
-#                         print '======================Â≠ê‰ªªÂä°Â∑≤ÁîüÊàê====================='
-#                     except Exception,error:
-#                         print "==============Â≠ê‰ªªÂä°ÁîüÊàêÂ§±Ë¥• %s===================" % error
-#                 else:
-#                     print "==================PlaybookList is Null============"
-#             except Exception,error:
-#                 print '====================Create mission failed! %s======================' % error
-#         except Exception,error:
-#             print '===============No host or playbook selected! %s==========================' % error
-#         return render(request,'basex.html',{'sdf':(PlaybookList,HostList,Version)})
-#     else:
-#         return render(request,'basex.html',{'sdf':'error'})
-#
-# def Write_to_redis(request):
-#     dics = {}
-#     if request.method == 'POST':
-#         dics = eval(json.dumps(request.POST))
-#         try:
-#             for k,val in dics.items():
-#                 print k,val
-#                 if k != 'csrfmiddlewaretoken':
-#                     cache.set(k,val)
-#         except Exception,e:
-#             return render(request,'basex.html',{'sdf':dics})
-#
-#
-# def Get_from_redis(request,*args):
-#     dicc = {}
-#     for i in args:
-#         try:
-#             dicc[i] = cache.get(i)
-#         except Exception,e:
-#             return render(request,'basex.html',{'sdf':e})
-#     return render(request,'basex.html',{'sdf':dicc})
-#
-# def basex(request):
-#     dicx = {}
-#     if request.method == 'POST':
-#         dicx = eval(json.dumps(request.POST))
-#         try:
-#             for k,val in dicx.items():
-#                 print k,val
-#                 if k != 'csrfmiddlewaretoken':
-#                     cache.set(k,val)
-#         except Exception,e:
-#             return  render(request,'basex.html',{'sdf':e})
-#     return render(request,'basex.html',{'sdf':dicx})
->>>>>>> 0ad38c48428e7d10c83df9c3bd31140474f67a82
+
+def index(request):
+    return render(request,'index.html')
+
+
+def CreateMission(request):
+    if request.method == 'GET':
+        return render(request,'index.html')
+    elif request.method == 'POST':
+        HostList = list(set([ host for host in request.REQUEST.get('hostlist').split(',')]))
+        PlaybookList = list(set([playbook for playbook in request.REQUEST.get('playbook').split(',')]))
+        Version = request.REQUEST.get('version')
+        UniqueValue = datetime.now().strftime("%Y%m%d%H%M%S")
+        status = {
+            '0':'unexecuted',
+            '1':'executing',
+            '2':'executed',
+            '3':'failed',
+            '4':'unknown',
+        }
+        remark = request.REQUEST.get('remark')
+        try:
+            print '=====================ÂºÄÂßãÁîüÊàê‰∏ª‰ªªÂä°==================='
+            Miss.objects.create(
+                hosts=HostList,
+                playbooks=PlaybookList,
+                version=Version,
+                status=status['0'],
+                uniquevalue=UniqueValue,
+                release_time=datetime.now(),
+                finish_time='unknown',
+                remark=remark
+            )
+            print '======================‰∏ª‰ªªÂä°Â∑≤ÁîüÊàê====================='
+            for host in HostList:
+                hostdic = {'host':host,
+                           'status':status['0'],
+                           'playbook':PlaybookList
+                           }
+                try:
+                    SubMiss.objects.create(
+                        host=host,
+                        playbooks=PlaybookList,
+                        release_time=datetime.now(),
+                        finish_time='unknown',
+                        status=status['0'],
+                        uniquevalue=UniqueValue
+                    )
+                    print '======================Â≠ê‰ªªÂä°ÂÜôÂÖ•mysql====================='
+                    try:
+                        WriteToEtcd(host,hostdic)
+                        print '======================Â≠ê‰ªªÂä°Â∑≤ÂÜôÂÖ•etcd====================='
+                    except Exception,error:
+                        print '======================ÂÜôÂÖ•etcdÂ§±Ë¥• %s=====================' % error
+                except Exception,error:
+                    print "==============Â≠ê‰ªªÂä°ÁîüÊàêÂ§±Ë¥• %s===================" % error
+        except Exception,error:
+            print '====================Create mission failed! %s======================' % error
+        return render(request,'basex.html',{'sdf':(PlaybookList,HostList,Version)})
+    else:
+        return render(request,'basex.html',{'sdf':'use post or get'})
+
+
+def basex(request):
+    dicx = {}
+    if request.method == 'POST':
+        dicx = eval(json.dumps(request.POST))
+        try:
+            for k,val in dicx.items():
+                print k,val
+                if k != 'csrfmiddlewaretoken':
+                    cache.set(k,val)
+        except Exception,e:
+            return  render(request,'basex.html',{'sdf':e})
+    return render(request,'basex.html',{'sdf':dicx})
+
+
+
+
+
